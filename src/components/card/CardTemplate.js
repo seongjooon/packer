c958039effa15ec69914ab01a2dac7e678ebf06a
MrKamake <seongjooon@gmail.com>
2020-04-21 14:11:07 +0900

    Style Card template

--- src/components/card/CardTemplate.js
+++ src/components/card/CardTemplate.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import './CardTemplate.scss';
 import ProductCard from './ProductCard';
 import ReviewCard from './ReviewCard';
 import CoverImg from '../../images/coverimg.png';
 
 const CardTemplate = () => {
   return (
-    <div>
+    <div className="Card-Template">
       <ProductCard
         created_by="Card Label"
         title="Card Title"
         coverImage={CoverImg}
         rating={3}
-        comment="Lorem ipsum dolor sit amet consectetur adipisicing elit"
+        comment="Lorem ipsum dolor sit amet consectetur adipisicing elits graitudegupo"
       />
       <ProductCard
         created_by="Card Label"
@@ -26,10 +27,10 @@ const CardTemplate = () => {
       />
       <ReviewCard
         reviewer="John Doe"
-        title="Review Title"
+        title="Lorem ipsum dolor sit amet consectetur adipisicing elits graitudegupo"
         coverImage={CoverImg}
         rating={3}
-        comment="Lorem ipsum dolor sit amet consectetur adipisicing elit"
+        comment="Lorem ipsum dolor sit amet consectetur adipisicing elits graitudegupo"
       />
     </div>
   );
