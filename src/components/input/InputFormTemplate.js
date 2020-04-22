23ce78c9cb88f8aef96828db775a7fe6610b189b
MrKamake <seongjooon@gmail.com>
2020-04-22 16:04:14 +0900

    Style InputFormTamplate

--- src/components/input/InputFormTemplate.js
+++ src/components/input/InputFormTemplate.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './InputFormTemplate.scss';
 
 const InputFormTemplate = ({ children }) => {
-  return <div className="InputForm-Template">{children}</div>;
+  return (
+    <div className="InputForm-Template">
+      <div className='InputForm_wrapper'>{children}</div>
+    </div>
+  );
 };
 
 export default InputFormTemplate;
